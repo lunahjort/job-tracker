f9bb50560c927339593828804c9cc32e2d5d178b
Luna <lunahjort@hotmail.com>
2026-05-12 00:20:12 +0200

    lagt till POST + GET i main

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,27 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+class Job(BaseModel):
+    company: str
+    role: str
+    status: str
 
 app = FastAPI()
+
+# fake databas, endast lista
+jobs = []
 
 @app.get("/")
 def root():
     return {"message": "API is running"}
+
+# GET /jobs
+@app.get("/jobs")
+def get_jobs():
+    return jobs
+
+# POST /jobs
+@app.post("/jobs")
+def add_job (job: Job):
+    jobs.append(job)
+    return {"message": "Job added"}
